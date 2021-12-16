4f625e5e85073753f4444a877f71f2d72d439d92
bpolicarpio <85565508+bpolicarpio@users.noreply.github.com>
2021-12-15 22:31:50 -0800

    updated template, added model

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Weather(models.Model):
+    weather = models.BooleanField()
+    feeling = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.date}"
--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import WeatherForm
+
+from .models import Weather
 
 # Create your views here.
 def index(request):
-    form = WeatherForm
-    return render(request, "weather/index.html", {
-        "form": form
-    })
+    if request.method == "POST":
+        if "feeling" in request.POST and "weather" in request.POST:
+            feeling = request.POST["feeling"]
+            weather = request.POST["weather"]
+            if feeling == "happy":
+                feeling = True
+            else:
+                feeling = False
+            if weather == "good":
+                weather = True
+            else:
+                weather = False
+
+            Weather.objects.create(weather=weather, feeling=feeling)
+
+            return HttpResponse(f"Feeling: {feeling}, Weather: {weather}")
+        else:
+            return render(request, "weather/index.html", {
+                "message": "Please answer all questions."
+            })
+
+    return render(request, "weather/index.html")
 
 

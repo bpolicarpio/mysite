32ba1da91e09087464aebab7f7344d60127f3411
bpolicarpio <85565508+bpolicarpio@users.noreply.github.com>
2021-12-17 22:38:28 -0800

    added results tables

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Weather
+import matplotlib.pyplot as plt
 
 # Create your views here.
 def index(request):
@@ -19,11 +20,9 @@ def index(request):
                 weather = False
 
             Weather.objects.create(weather=weather, feeling=feeling)
-            results = Weather.objects.all()
 
-            return render(request, "weather/results.html", {
-                "results": results
-            })
+            return redirect('weather:results')
+
         else:
             return render(request, "weather/index.html", {
                 "message": "Please answer all questions."
@@ -31,4 +30,33 @@ def index(request):
 
     return render(request, "weather/index.html")
 
+def results(request):
+    results = Weather.objects.all()
+    happy, sad, good, bad = 0, 0, 0, 0
+    for result in results:
+        if result.weather is True:
+            good += 1
+        else:
+            bad += 1
+        if result.feeling is True:
+            happy += 1
+        else:
+            sad +=1
 
+    weatherX = ['good', 'bad']
+    weatherY = [good, bad]
+    plt.bar(weatherX, weatherY)
+    plt.title('Weather')
+    plt.xlabel('weather')
+    plt.ylabel('quantity')
+    plt.savefig('/weather/static/weath.png')
+
+    feelingX = ['happy', 'sad']
+    feelingY = [happy, sad]
+    plt.bar(feelingX, feelingY)
+    plt.title('Feeling')
+    plt.xlabel('feeling')
+    plt.ylabel('quantity')
+    plt.savefig('/weather/static/feel.png')
+
+    return render(request, 'weather/results.html')
